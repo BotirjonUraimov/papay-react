59aaa69c165c340d85955e1c56bf1b1d4e37d316
BotirjonUraimov <botirjon.uraimov.93@gmail.com>
2023-02-19 06:59:06 +0900

    BRR feat: checking react router dom

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,9 +2,54 @@ import React from "react";
 import "../css/App.css";
 import { Box, Button, Container, Stack, Typography } from "@mui/material";
 import { RippleBadge } from "./MaterialTheme/styled";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
-  return <Container></Container>;
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/Dishes">Dishes</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/Dishes">
+            <Dishes />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
+
+function Users() {
+  return <h2>Users</h2>;
+}
+
+function Dishes() {
+  return <h2>Dishes</h2>;
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
